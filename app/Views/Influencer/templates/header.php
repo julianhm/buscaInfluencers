beb8f41198c4e8cc7190da0d05ea1f6263f53564
Leonardo <leo.ram2007@gmail.com>
2023-02-16 18:06:03 -0500

    se organizan los css en local y de los headers

--- app/Views/Influencer/templates/header.php
+++ app/Views/Influencer/templates/header.php
@@ -13,20 +13,23 @@
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
-
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet"> -->
+    <link href=<?php echo base_url('css/opensans.css');?> rel="stylesheet">
     <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">-->
+    <link href=<?php echo base_url('css/all.min.css');?> rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"> -->
+    <link href=<?php echo base_url('css/bootstrap-icons.css');?> rel="stylesheet">
         <!-- Customized Bootstrap Stylesheet -->
     <link href=<?php echo base_url('css/bootstrap.min.css');?> rel="stylesheet">
 
     <!-- Template Stylesheet -->
     <link href=<?php echo base_url('css/style.css');?> rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.1.4/pagination.min.js"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.1.4/pagination.min.js"></script>-->
+    <script src=<?php echo base_url('js/pagination.min.js');?>></script>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.1.4/pagination.css"/>
+<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.1.4/pagination.css"/> -->
+    <link href=<?php echo base_url('css/pagination.css');?> rel="stylesheet">
 
   <link rel='stylesheet' href=<?php echo base_url('lib/owlcarousel/assets/owl.carousel.min.css');?>>
   <link rel='stylesheet' href=<?php echo base_url('lib/owlcarousel/assets/owl.theme.green.min.css');?>>
--- app/Views/Influencer/templates/headerindex.php
+++ app/Views/Influencer/templates/headerindex.php
@@ -10,11 +10,14 @@
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet"> -->
+    <link href=<?php echo base_url('css/opensans.css');?> rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">-->
+    <link href=<?php echo base_url('css/all.min.css');?> rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"> -->
+    <link href=<?php echo base_url('css/bootstrap-icons.css');?> rel="stylesheet">
 
         <!-- Customized Bootstrap Stylesheet -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
