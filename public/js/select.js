c145062f33141eb41236c4882d673ce7c35a574b
julianchohm <julianchohm@hotmail.com>
2023-06-11 09:08:56 -0500

    incorporar cambios

--- public/js/select.js
+++
@@ -1,8 +0,0 @@
-$("#pais").change(function() {
-    if($("#pais").val() !== "0"){
-      $('#ciudades').prop('disabled', false);
-    }else{
-        $('#ciudades').prop('disabled', 'disabled');
-      
-    }
-  });
